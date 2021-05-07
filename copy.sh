f27f506aeedbac925fedd1b3f6ca737af6aacdf7
csfankun <3312694364@qq.com>
2021-05-07 12:03:09 +0800

    完成5个文件夹的部署

---
+++ copy.sh
@@ -0,0 +1,57 @@
+if [ -d "jsj-antr/" ];then
+  rm -rf jsj-antr
+else
+  echo "jsj-antr文件夹不存在"
+fi
+
+if [ -d "jsj-antv/" ];then
+  rm -rf jsj-antv
+else
+  echo "jsj-antv文件夹不存在"
+fi
+
+if [ -d "jsj-common/" ];then
+  rm -rf jsj-common
+else
+  echo "jsj-common文件夹不存在"
+fi
+
+if [ -d "jsj-ele/" ];then
+  rm -rf jsj-ele
+else
+  echo "jsj-ele文件夹不存在"
+fi
+
+if [ -d "jsj-vue/" ];then
+  rm -rf jsj-vue
+else
+  echo "jsj-vue文件夹不存在"
+fi
+
+cd ..
+cd npm-packages
+
+cd jsj-antr
+npm run build:doc
+scp -r styleguide ../../FrontStandard/jsj-antr
+
+cd ..
+cd jsj-vue
+npm run build:doc
+scp -r dist ../../FrontStandard/jsj-vue
+
+cd ..
+cd jsj-antv
+npm run build:doc
+scp -r dist ../../FrontStandard/jsj-antv
+
+cd ..
+cd jsj-common
+npm run build:doc
+scp -r styleguide ../../FrontStandard/jsj-common
+
+cd ..
+cd jsj-ele
+npm run build:doc
+scp -r dist ../../FrontStandard/jsj-ele
+
